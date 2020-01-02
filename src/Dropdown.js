1235a7efde7200a98ae7f4858f3fb4ac0f9ce447
Flower92 <liliana.avtonasova@gmail.com>
2020-01-02 23:08:10 +0100

    EPNG-0001 Initial commit

---
+++ src/Dropdown.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class Dropdown extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isOpened: false };
+  }
+  toggleState() {
+    this.setState({ isOpened: !this.state.isOpened});
+  }
+  render() {
+    console.log( 'isOpened', this.state.isOpened);
+    let dropdownText;
+    if (this.state.isOpened) {
+      dropdownText = <div>Dropdown text is here</div>;
+    }
+    return (
+      <div onClick={this.toggleState.bind(this)}>
+        <p>It is Dropdown</p>
+        {dropdownText}
+    </div>
+    );
+  }
+}
+
+export default Dropdown;
