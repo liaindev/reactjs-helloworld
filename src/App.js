da810e3497ab31ed712372a57a0f9b8ee6450a9c
Flower92 <liliana.avtonasova@gmail.com>
2020-01-07 23:13:11 +0100

    EPNG-0005 Created registration form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React, {Component} from 'react';
-import Header from './Header';
-//import Dropdown from './Dropdown';
-
-
-const menu = [
-  {
-    link: '/articles',
-    label: 'Articles'
-  },
-  {
-    link: '/contacts',
-    label: 'Contacts'
-  },
-  {
-    link: '/posts',
-    label: 'Posts'
-  }
-];
+import RegistrationForm from './RegistrationForm';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header items={menu}/>
+        <RegistrationForm />
       </div>
     );
   }
