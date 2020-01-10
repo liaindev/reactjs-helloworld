79e1ae66e66c9d6a794ac0c3c672b073d41694bb
Flower92 <liliana.avtonasova@gmail.com>
2020-01-10 20:35:54 +0100

    EPNG-0006_new_lesson Created new files RegistrationForm.css & App.css

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './RegistrationForm.css';
 
 class RegistrationForm extends Component {
   constructor(props) {
@@ -28,8 +29,9 @@ class RegistrationForm extends Component {
           placeholder="E-mail"
           value={this.state.email}
           onChange={this.handleEmailChange}
+          className="emailField"
         />
-        <button>Save</button>
+        <button className="submitButton">Save</button>
       </form>
     );
   }
