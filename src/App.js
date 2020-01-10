405e797d2551935565c0d9e50ab9004aec5295d1
Flower92 <liliana.avtonasova@gmail.com>
2020-01-11 00:31:57 +0100

    EPNG-0007 Refs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-import "./App.css";
-
-import RegistrationForm from './RegistrationForm';
 
 class App extends Component {
+  submit() {
+    console.log('submit', this.testInput.value);
+  }
   render() {
     return (
-      <div className="container">
-        <RegistrationForm />
+      <div>
+        <input type="text" placeholder="test" ref={(input) => this.testInput = input}/>
+        <button onClick={this.submit.bind(this)}>Submit</button>
       </div>
     );
   }
